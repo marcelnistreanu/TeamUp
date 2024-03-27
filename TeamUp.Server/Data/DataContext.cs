bb23c5ee6d498a0dc94f8fb4c4edf79724531eb8
marcelnistreanu <marcelnistreanu@gmail.com>
2024-03-27 20:32:29 +0200

    Add database connection

---
+++ TeamUp.Server/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamUp.Server.Models;
+
+namespace TeamUp.Server.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Player> Players { get; set; }
+}
---
+++ TeamUp.Server/Migrations/20240327150009_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamUp.Server.Migrations;
+
+/// <inheritdoc />
+public partial class Initial : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Players",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false)
+                    .Annotation("Sqlite:Autoincrement", true),
+                Name = table.Column<string>(type: "TEXT", nullable: false),
+                NickName = table.Column<string>(type: "TEXT", nullable: false),
+                Age = table.Column<int>(type: "INTEGER", nullable: false),
+                Rating = table.Column<int>(type: "INTEGER", nullable: false),
+                PreferredGame = table.Column<string>(type: "TEXT", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Players", x => x.Id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Players");
+    }
+}
