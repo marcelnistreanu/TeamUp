0229b43b72318f684d57ff67533602b4355906ec
marcelnistreanu <marcelnistreanu@gmail.com>
2024-04-23 01:12:36 +0300

    Add side-bar

--- teamup.client/src/app/app.component.ts
+++ teamup.client/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TeamUp.Client';
+  title = 'TeamUp!';
 }
